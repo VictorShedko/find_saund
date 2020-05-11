b34f758f2ce75e970296e89eb40ac56ecd20724f
Victor <vitoreshedko@gmail.com>
2020-05-12 01:17:09 +0300

    added half-pseudocode realization

--- lol.py
+++ lol.py
@@ -3,20 +3,36 @@ import numpy as np
 import librosa.display
 import scipy
 from scipy import stats
-
+import IPython.display as ipd
 
 def get_library():
     library = []
     name_file = open("names.txt")
     for line in name_file:
-        y, sr = librosa.load("sound_libary/" + line, sr=None)
-        library.append(y)
+        y, sr = librosa.load("sound_libary/" + line, sr=None)#todo delete /n at the end of raw
+        library.append([y, sr])
     return library
 
 
+def get_winner(sound, lib):
+    cor_max = 0
+    cor_i = 0
+    for i in range(lib):
+        sample = lib[i][0]
+        cor = np.correlate(sample, sound)
+        if max(cor) > cor_max:
+            cor_max = max(cor)
+            cor_i = i
+    return cor_i, lib[cor_i]
+
+
 if __name__ == '__main__':
-    lib = get_library()  # y то шо нам нужно чистая как какаин вейформа sr частота дискретизации
-    
+    ipd.Audio("sound_libary/system.wav")
+    y, sr = librosa.load("sound_libary/system.wav", sr=None)  # в идеале с микро взять
+    lib = get_library()  # y- то шо нам нужно чистая как какаин вейформа sr частота дискретизации
+
+    num_in_libary, gused_track = get_winner(y, lib)
+
     # куча таких это библиотека будет
     # какая то входная последовательнасть можно с микро можно просто кусок
 
